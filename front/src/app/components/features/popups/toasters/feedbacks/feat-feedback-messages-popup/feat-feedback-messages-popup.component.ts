0b138edf515a9771c4a5e9f8efdf5f2c5fe09df3
williamh973 <126678879+williamh973@users.noreply.github.com>
2024-04-19 13:13:05 +0200

    fix feedback message bug for contact page

--- front/src/app/components/features/popups/toasters/feedbacks/feat-feedback-messages-popup/feat-feedback-messages-popup.component.ts
+++ front/src/app/components/features/popups/toasters/feedbacks/feat-feedback-messages-popup/feat-feedback-messages-popup.component.ts
@@ -21,6 +21,7 @@ export class FeatFeedbackMessagesPopupComponent {
   ngOnInit() {
     this.onShowNewsCardFeedback();
     this.onShowLocationFeedback();
+    this.onShowContactTextFeedback();
   }
   
   
